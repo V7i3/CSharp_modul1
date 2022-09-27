5e5beec12a295cfef62e6e44cc66c44a60e54e59
V7i3 <violetta.mitkovskaya@gmail.com>
2022-09-27 18:04:23 +0300

    Task5*

---
+++ Lesson2/Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 5 *: Напишите программу, которая генерирует несколько случайных чисел, и в цикле проверяет, 
+кратны ли эти числа предварительно введенному числу, при кратности - цикл прерывается.
+Введенное число 2 */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Random rnd = new Random();
+int[] array = new int[] {rnd.Next(0, 20), rnd.Next(0, 20), rnd.Next(0, 20), rnd.Next(0, 20), rnd.Next(0, 20),};
+
+for (int i = 0; i < array.Length; i++){
+    if (array[i]%number == 0){
+        Console.WriteLine(array[i] + " -> Да");
+        break;
+    }
+    else  
+       Console.WriteLine(array[i] + " -> Нет");
+}
