f4a862ae55e7dd401b2e4bf5c22a66701e8749a6
V7i3 <violetta.mitkovskaya@gmail.com>
2022-09-27 16:18:47 +0300

    Lesson3

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] nums = new int[5];
+
+if (number <= 99999 || number >= 10000)
+{
+    Array(nums, number);
+    if ( nums[0] == nums[4] && nums[1] == nums[3])
+        Console.WriteLine("Вы ввели палиндром");
+    else
+        Console.WriteLine("Вы ввели не палиндром");
+}
+else 
+     Console.WriteLine("Вы ввели не пятизначное число!");     
+
+void Array(int[] arr, int num){
+    arr[0] = num%10;
+    arr[1] = num%100/10;
+    arr[2] = num%1000/100;
+    arr[3] = num%10000/1000; 
+    arr[4] = num/10000;
+}
---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[number+1];
+Cube(arr);
+PrintArr(arr);
+
+void Cube(int[] number){
+  for (int i = 0; i <  number.Length; i++){
+    number[i] = Convert.ToInt32(Math.Pow(i, 3));
+  }
+}
+
+void PrintArr(int[] array){
+  for(int i = 1; i < array.Length; i++){
+    if (i+1 >= array.Length)
+       Console.Write(array[i]);
+    else
+       Console.Write(array[i]+ ",");
+  }
+} 
