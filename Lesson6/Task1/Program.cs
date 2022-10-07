72f201b5c838ee5f17cbc063e1c91ec209c258eb
V7i3 <violetta.mitkovskaya@gmail.com>
2022-10-07 17:04:09 +0300

    Lesson6

---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 1: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь. */
+
+int[] FillArray(int amount){
+    
+    int[] arr = new int[amount];
+    for ( int i = 0; i < arr.Length; i++){
+        Console.Write($"Введите {i + 1} число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int AboveZero(int[] array){
+
+    int count = 0;
+
+   for (int i = 0; i < array.Length; i++){
+        if (array[i] > 0){
+            count++;
+        }
+   }
+
+   return count;
+
+}
+
+Console.Write("Введите количество чисел: ");
+int[] array = FillArray(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine($"Количество чисел больше нуля = {AboveZero(array)} ");
---
+++ Lesson6/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем. */
+
+
+ string CrossingBeeline(double k1, double b1, double k2, double b2){
+
+    double x = (b2-b1)/(k1-k2);
+    double y = k1*(b2-b1)/(k1-k2) + b1;
+    return $"({x}, {y})";
+ }
+
+void PrintEqation(int item, double k, double b){
+    Console.WriteLine($"y{item} = {k}x + {b}");
+}
+
+ int Coordinates(int num, char letter){
+    Console.Write($"Введите {letter}{num}: ");
+    return Convert.ToInt32(Console.ReadLine());
+ }
+
+
+double b1 = Coordinates(1, 'b');
+double k1 = Coordinates(1, 'k');
+double b2 = Coordinates(2, 'b');
+double k2 = Coordinates(2, 'k');
+
+Console.WriteLine();
+
+PrintEqation(1, k1, b1);
+PrintEqation(2, k2, b2);
+
+Console.WriteLine("Координаты: " + CrossingBeeline(k1, b1, k2, b2));
