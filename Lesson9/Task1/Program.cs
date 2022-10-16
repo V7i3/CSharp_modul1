60d05208ed854bafffe38b39d22dca20c04d6ace
V7i3 <violetta.mitkovskaya@gmail.com>
2022-10-16 12:58:41 +0300

    Lesson9

---
+++ Lesson9/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 1: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+
+void Numbers(int amount){
+    if(amount == 0) return ; 
+    else{
+       Console.Write($"{amount--} ");
+       Numbers(amount);
+    }
+}
+
+Console.Write("Введите максимальное число: ");
+int amt = Convert.ToInt32(Console.ReadLine());
+
+Numbers(amt);
---
+++ Lesson9/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*  Задача 2: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int NumbersSum(int start, int finish, int sum){
+    if(start <= finish) {
+        sum += start;
+        return NumbersSum(start + 1, finish, sum);
+    }
+    else return sum;
+}
+
+Console.Write("Введите начальное число: ");
+int start = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конечное число: ");
+int finish = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Сумма целых чисел в диапазоне [{start},{finish}] = {NumbersSum(start, finish, 0)}");
+
+
